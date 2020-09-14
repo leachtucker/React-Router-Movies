7b33ff787e0d52569ea994fe141b1f18b96fc3ec
Tucker Leach <leachtucker@gmail.com>
2020-09-14 14:51:48 -0700

    Completed routing and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
-  const [movieList, setMovieList] = useState([]);
+  const [saved, setSaved] = useState(); // Stretch: the ids of "saved" movies
+  const [movieList, setMovieList] = useState(null);
 
   useEffect(() => {
     const getMovies = () => {
@@ -14,6 +17,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -26,11 +30,21 @@ export default function App () {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
+  if (!movieList) {
+    return (
+      <h2>"Fetching data!.."</h2>
+    );
+  }
+
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
       <div>Replace this Div with your Routes</div>
+
+      <Route exact path="/" component={() => <MovieList movies={movieList} />} />
+      <Route path="/movies/:id" component={Movie} />
+
     </div>
   );
 }
